fe4b3f36ccc8ffd3e0959179b49f4408080a4c5e
samithiwat <samithiwat2544@gmail.com>
2022-05-12 20:04:11 +0700

    Implement create function

--- src/auth/services/token.service.ts
+++ src/auth/services/token.service.ts
@@ -1,11 +1,46 @@
-import { Injectable } from '@nestjs/common';
-import { CreateAuthDto } from '../dto/create-auth.dto';
+import { HttpStatus, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateTokenDto } from '../dto/create-token.dto';
+import { ResponseDto } from '../dto/response.dto';
 import { UpdateAuthDto } from '../dto/update-auth.dto';
+import { Token } from '../entities/token.entity';
+import { TokenResponse } from '../interface/token.interface';
+import { JwtService } from './jwt.service';
+import { RefreshTokenService } from './refresh-token.service';
 
 @Injectable()
 export class TokenService {
-  create(createAuthDto: CreateAuthDto) {
-    return 'This action adds a new auth';
+  constructor(
+    @InjectRepository(Token) private readonly tokenRepository: Repository<Token>,
+    private readonly jwtService: JwtService,
+    private readonly refreshTokenService: RefreshTokenService,
+  ) {}
+
+  async create(createTokenDto: CreateTokenDto): Promise<TokenResponse> {
+    const res = new ResponseDto({
+      statusCode: HttpStatus.CREATED,
+      errors: null,
+      data: null,
+    }) as TokenResponse;
+
+    createTokenDto.accessToken = createTokenDto.accessToken
+      ? createTokenDto.accessToken
+      : await this.jwtService.generate(createTokenDto.auth);
+
+    createTokenDto.refreshToken = createTokenDto.refreshToken
+      ? createTokenDto.refreshToken
+      : await this.refreshTokenService.generate();
+
+    try {
+      const token = await this.tokenRepository.save(createTokenDto);
+      res.data = token;
+      return res;
+    } catch (err) {
+      res.statusCode = HttpStatus.UNPROCESSABLE_ENTITY;
+      res.errors = ['Refresh token already exists'];
+      return res;
+    }
   }
 
   findAll() {
