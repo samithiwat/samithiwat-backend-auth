b44fbdeae78b68a81bfc8ae6555733da4464bae9
samithiwat <samithiwat2544@gmail.com>
2022-05-12 15:02:59 +0700

    Change auth service directory

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,8 @@
 import { Controller } from '@nestjs/common';
 import { MessagePattern, Payload } from '@nestjs/microservices';
-import { AuthService } from './auth.service';
 import { CreateAuthDto } from './dto/create-auth.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
+import { AuthService } from './services/auth.service';
 
 @Controller()
 export class AuthController {
