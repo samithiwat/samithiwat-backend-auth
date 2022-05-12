6aa4e6f89177b7fec959834e78a03d04002f486e
samithiwat <samithiwat2544@gmail.com>
2022-05-12 15:01:28 +0700

    Create base entity

---
+++ src/common/entity/base.entity.ts
@@ -0,0 +1,24 @@
+import {
+  CreateDateColumn,
+  DeleteDateColumn,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+export abstract class BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @CreateDateColumn({ name: 'created_date', select: false })
+  createdDate: Date;
+
+  @UpdateDateColumn({ name: 'updated_date', select: false })
+  updatedDate: Date;
+
+  @DeleteDateColumn({ name: 'deleted_date', select: false })
+  deletedDate: Date;
+
+  constructor(partial: Partial<BaseEntity>) {
+    Object.assign(this, partial);
+  }
+}
