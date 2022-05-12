9def7c3856cbbb0eaa95f9c46fc4657032fee70d
samithiwat <samithiwat2544@gmail.com>
2022-05-12 15:10:01 +0700

    Create refresh token service and add to module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './services/auth.service';
+import { RefreshTokenService } from './services/refresh-token.service';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, RefreshTokenService],
 })
 export class AuthModule {}
