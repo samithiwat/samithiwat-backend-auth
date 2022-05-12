a37a3269efc9c7dd075c9b45a74fe679503dcdbc
samithiwat <samithiwat2544@gmail.com>
2022-05-12 14:03:54 +0700

    Implement grpc service in main

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { join } from 'path';
 import { AppModule } from './app.module';
+import config from './config/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const logger = new Logger('Bootstrap');
+
+  const appConfig = config().app;
+  const host = appConfig.host;
+  const port = appConfig.port;
+
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+    transport: Transport.GRPC,
+    options: {
+      url: `${host}:${port}`,
+      package: ['auth', 'dto'],
+      protoPath: join(__dirname, 'proto/auth.proto'),
+    },
+  });
+
+  await app.listen();
+  logger.log(`Starting Samithiwat's auth service listening at port ${port}`);
 }
 bootstrap();
