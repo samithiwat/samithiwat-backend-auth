a43b78c28aa033a19cdc2268a1ff42e2a6449b1b
samithiwat <samithiwat2544@gmail.com>
2022-05-12 15:01:07 +0700

    Create auth resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
 import config from './config/config';
 
 @Module({
@@ -24,6 +25,7 @@ import config from './config/config';
       }),
       inject: [ConfigService],
     }),
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,35 @@
+import { Controller } from '@nestjs/common';
+import { MessagePattern, Payload } from '@nestjs/microservices';
+import { AuthService } from './auth.service';
+import { CreateAuthDto } from './dto/create-auth.dto';
+import { UpdateAuthDto } from './dto/update-auth.dto';
+
+@Controller()
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @MessagePattern('createAuth')
+  create(@Payload() createAuthDto: CreateAuthDto) {
+    return this.authService.create(createAuthDto);
+  }
+
+  @MessagePattern('findAllAuth')
+  findAll() {
+    return this.authService.findAll();
+  }
+
+  @MessagePattern('findOneAuth')
+  findOne(@Payload() id: number) {
+    return this.authService.findOne(id);
+  }
+
+  @MessagePattern('updateAuth')
+  update(@Payload() updateAuthDto: UpdateAuthDto) {
+    return this.authService.update(updateAuthDto.id, updateAuthDto);
+  }
+
+  @MessagePattern('removeAuth')
+  remove(@Payload() id: number) {
+    return this.authService.remove(id);
+  }
+}
