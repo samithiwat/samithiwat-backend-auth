740a279ee4fbfc731d23560262dd5e4c7cc35927
samithiwat <samithiwat2544@gmail.com>
2022-05-12 15:08:32 +0700

    Create token service file and add to module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './services/auth.service';
+import { TokenService } from './services/token.service';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, TokenService],
 })
 export class AuthModule {}
