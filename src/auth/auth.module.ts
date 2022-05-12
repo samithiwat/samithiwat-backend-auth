e096ae044a0f073fc6e5a9934b61d067f077e51f
samithiwat <samithiwat2544@gmail.com>
2022-05-12 15:05:32 +0700

    Create jwt service file and add to module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './services/auth.service';
+import { JwtService } from './services/jwt.service';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtService],
 })
 export class AuthModule {}
