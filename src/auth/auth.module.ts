fc3665ce3d7d78aede0cbfc9ccd46fb582625b1e
samithiwat <samithiwat2544@gmail.com>
2022-05-12 16:34:42 +0700

    Implement jwt service and add to module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './auth.controller';
 import { AuthService } from './services/auth.service';
 import { JwtService } from './services/jwt.service';
 
 @Module({
+  imports: [
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('jwt.secret'),
+        signOptions: { expiresIn: configService.get<string>('jwt.tokenDuration') },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService, JwtService],
 })
--- src/auth/services/jwt.service.ts
+++ src/auth/services/jwt.service.ts
@@ -1,26 +1,30 @@
 import { Injectable } from '@nestjs/common';
-import { CreateAuthDto } from '../dto/create-auth.dto';
-import { UpdateAuthDto } from '../dto/update-auth.dto';
+import { JwtService as Jwt } from '@nestjs/jwt';
+import { InjectRepository } from '@nestjs/typeorm';
+import { TokenPayload } from 'src/common/types/auth';
+import { Repository } from 'typeorm';
+import { Auth } from '../entities/auth.entity';
 
 @Injectable()
 export class JwtService {
-  create(createAuthDto: CreateAuthDto) {
-    return 'This action adds a new auth';
+  constructor(
+    @InjectRepository(Auth) private authRepository: Repository<Auth>,
+    private jwtService: Jwt,
+  ) {}
+
+  async decode(token: string): Promise<TokenPayload> {
+    return this.jwtService.decode(token) as TokenPayload;
   }
 
-  findAll() {
-    return `This action returns all auth`;
+  async findFromPayload(decode: TokenPayload): Promise<Auth> {
+    return this.authRepository.findOne({ id: decode.id });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} auth`;
+  async generateToken(auth: Auth): Promise<string> {
+    return this.jwtService.sign({ id: auth.id });
   }
 
-  update(id: number, updateAuthDto: UpdateAuthDto) {
-    return `This action updates a #${id} auth`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} auth`;
+  async verifyToken(token: string): Promise<TokenPayload> {
+    return this.jwtService.verify(token);
   }
 }
