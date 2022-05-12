1a5f1fa836b63b5af14798b7bc6835eff741de33
samithiwat <samithiwat2544@gmail.com>
2022-05-12 14:03:39 +0700

    Setup app config file

---
+++ src/config/config.ts
@@ -0,0 +1,25 @@
+import * as dotenv from 'dotenv';
+
+const envType = process.env.NODE_ENV || 'dev';
+
+dotenv.config({ path: `.env.${envType}` });
+
+export default () => ({
+  app: {
+    host: process.env.HOST || 'localhost',
+    port: parseInt(process.env.PORT) || 3001,
+    origin: true,
+  },
+  jwt: {
+    secret: process.env.JWT_SECRET,
+    tokenDuration: process.env.TOKEN_DURATION || '3600s',
+  },
+  database: {
+    type: process.env.DATABASE_TYPE,
+    host: process.env.DATABASE_HOST || 'localhost',
+    port: parseInt(process.env.DATABASE_PORT),
+    name: process.env.DATABASE_NAME,
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+  },
+});
