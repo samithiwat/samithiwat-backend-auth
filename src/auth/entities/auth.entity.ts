9c1985d892a8efcadf251e51acc6494724c992b2
samithiwat <samithiwat2544@gmail.com>
2022-05-12 15:01:34 +0700

    Create auth entity

---
+++ src/auth/entities/auth.entity.ts
@@ -0,0 +1,23 @@
+import { BaseEntity } from 'src/common/entity/base.entity';
+import { Column, Entity, Index } from 'typeorm';
+
+@Entity()
+export class Auth extends BaseEntity {
+  @Index({ unique: true })
+  @Column({ nullable: false })
+  email: string;
+
+  @Column({ nullable: false })
+  password: string;
+
+  @Column({ name: 'is_email_verified', default: false })
+  isEmailVerified: boolean;
+
+  @Column({ name: 'user_id', nullable: true })
+  userId: number;
+
+  constructor(partial: Partial<BaseEntity>) {
+    super(partial);
+    Object.assign(this, partial);
+  }
+}
