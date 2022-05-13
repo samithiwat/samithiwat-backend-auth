92b10ad650478b8d733a809cdbd38884cdf4d8e9
samithiwat <samithiwat2544@gmail.com>
2022-05-13 23:49:01 +0700

    Remove unused code

--- src/auth/services/token.service.spec.ts
+++ src/auth/services/token.service.spec.ts
@@ -1,7 +1,6 @@
 import { faker } from '@faker-js/faker';
 import { HttpStatus } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { ServiceType } from 'src/common/enum/auth.enum';
@@ -40,16 +39,7 @@ describe('TokenService', () => {
 
   beforeEach(async () => {
     const tokenModule: TestingModule = await Test.createTestingModule({
-      imports: [
-        JwtModule.registerAsync({
-          imports: [ConfigModule],
-          useFactory: async (configService: ConfigService) => ({
-            secret: configService.get<string>('jwt.secret'),
-            signOptions: { expiresIn: configService.get<string>('jwt.tokenDuration') },
-          }),
-          inject: [ConfigService],
-        }),
-      ],
+      imports: [],
       providers: [
         TokenService,
         {
