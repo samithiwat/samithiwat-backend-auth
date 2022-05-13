d5eab98d3a0bddf4802761166f2031be0bb80100
samithiwat <samithiwat2544@gmail.com>
2022-05-14 00:28:51 +0700

    Create user service interface and user response interface

--- src/user/user.interface.ts
+++ src/user/user.interface.ts
@@ -1,6 +1,21 @@
+import { BaseResponse } from 'src/common/interface/base.interface';
+import { CreateUserDto } from './dto/create-user.dto';
+
 export interface UserDto {
   firstname: string;
   lastname: string;
   displayName: string;
   imageUrl: string;
+}
+
+export interface UserService {
+  create(req: CreateUserRequest): UserResponse;
+}
+
+export interface CreateUserRequest {
+  createUserDto: CreateUserDto;
+}
+
+export interface UserResponse extends BaseResponse {
+  data: UserDto;
 }
