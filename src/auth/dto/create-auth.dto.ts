90a7d68234497ed9f9a94c623b41e4dbdcd5d644
samithiwat <samithiwat2544@gmail.com>
2022-05-13 23:33:07 +0700

    Implement create auth dto

--- src/auth/dto/create-auth.dto.ts
+++ src/auth/dto/create-auth.dto.ts
@@ -1,1 +1,8 @@
-export class CreateAuthDto {}
+export class CreateAuthDto {
+  email: string;
+  password: string;
+
+  constructor(partial: Partial<CreateAuthDto>) {
+    Object.assign(this, partial);
+  }
+}
