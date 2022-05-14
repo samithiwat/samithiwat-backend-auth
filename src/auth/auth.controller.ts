6db96e73a60e91f099434f4b03e0d22d80dd5680
samithiwat <samithiwat2544@gmail.com>
2022-05-14 22:37:42 +0700

    Implement auth controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,32 +1,52 @@
 import { Controller } from '@nestjs/common';
+import { GrpcMethod } from '@nestjs/microservices';
+import {
+  ChangePasswordRequest,
+  ChangePasswordResponse,
+  LoginRequest,
+  LoginResponse,
+  LogoutRequest,
+  LogoutResponse,
+  RefreshTokenRequest,
+  RefreshTokenResponse,
+  RegisterRequest,
+  RegisterResponse,
+  ValidateRequest,
+  ValidateResponse,
+} from './interface/auth.interface';
 import { AuthService } from './services/auth.service';
 
 @Controller()
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  // @MessagePattern('createAuth')
-  // create(@Payload() createAuthDto: CreateAuthDto) {
-  //   return this.authService.create(createAuthDto);
-  // }
+  @GrpcMethod('AuthService', 'Register')
+  async register(req: RegisterRequest): Promise<RegisterResponse> {
+    return this.authService.register(req.register);
+  }
 
-  // @MessagePattern('findAllAuth')
-  // findAll() {
-  //   return this.authService.findAll();
-  // }
+  @GrpcMethod('AuthService', 'Login')
+  async login(req: LoginRequest): Promise<LoginResponse> {
+    return this.authService.login(req.login);
+  }
 
-  // @MessagePattern('findOneAuth')
-  // findOne(@Payload() id: number) {
-  //   return this.authService.findOne(id);
-  // }
+  @GrpcMethod('AuthService', 'Logout')
+  async logout(req: LogoutRequest): Promise<LogoutResponse> {
+    return this.authService.logout(req.token);
+  }
 
-  // @MessagePattern('updateAuth')
-  // update(@Payload() updateAuthDto: UpdateAuthDto) {
-  //   return this.authService.update(updateAuthDto.id, updateAuthDto);
-  // }
+  @GrpcMethod('AuthService', 'Validate')
+  async changePassword(req: ChangePasswordRequest): Promise<ChangePasswordResponse> {
+    return this.authService.changePassword(req.changePassword);
+  }
 
-  // @MessagePattern('removeAuth')
-  // remove(@Payload() id: number) {
-  //   return this.authService.remove(id);
-  // }
+  @GrpcMethod('AuthService', 'Validate')
+  async validate(req: ValidateRequest): Promise<ValidateResponse> {
+    return this.authService.validate(req.token);
+  }
+
+  @GrpcMethod('AuthService', 'RedeemNewToken')
+  refreshToken(req: RefreshTokenRequest): Promise<RefreshTokenResponse> {
+    return this.authService.refreshToken(req.refreshToken);
+  }
 }
