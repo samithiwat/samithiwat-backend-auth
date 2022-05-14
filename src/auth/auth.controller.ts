24d39cd950766861f2d1470fff2cdb8eb60742e5
samithiwat <samithiwat2544@gmail.com>
2022-05-14 20:03:59 +0700

    Fix error

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,35 +1,32 @@
 import { Controller } from '@nestjs/common';
-import { MessagePattern, Payload } from '@nestjs/microservices';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
 import { AuthService } from './services/auth.service';
 
 @Controller()
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @MessagePattern('createAuth')
-  create(@Payload() createAuthDto: CreateAuthDto) {
-    return this.authService.create(createAuthDto);
-  }
+  // @MessagePattern('createAuth')
+  // create(@Payload() createAuthDto: CreateAuthDto) {
+  //   return this.authService.create(createAuthDto);
+  // }
 
-  @MessagePattern('findAllAuth')
-  findAll() {
-    return this.authService.findAll();
-  }
+  // @MessagePattern('findAllAuth')
+  // findAll() {
+  //   return this.authService.findAll();
+  // }
 
-  @MessagePattern('findOneAuth')
-  findOne(@Payload() id: number) {
-    return this.authService.findOne(id);
-  }
+  // @MessagePattern('findOneAuth')
+  // findOne(@Payload() id: number) {
+  //   return this.authService.findOne(id);
+  // }
 
-  @MessagePattern('updateAuth')
-  update(@Payload() updateAuthDto: UpdateAuthDto) {
-    return this.authService.update(updateAuthDto.id, updateAuthDto);
-  }
+  // @MessagePattern('updateAuth')
+  // update(@Payload() updateAuthDto: UpdateAuthDto) {
+  //   return this.authService.update(updateAuthDto.id, updateAuthDto);
+  // }
 
-  @MessagePattern('removeAuth')
-  remove(@Payload() id: number) {
-    return this.authService.remove(id);
-  }
+  // @MessagePattern('removeAuth')
+  // remove(@Payload() id: number) {
+  //   return this.authService.remove(id);
+  // }
 }
