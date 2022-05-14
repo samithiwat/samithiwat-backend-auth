6122c4e92431204b33aa36f33b63d26c07d10424
samithiwat <samithiwat2544@gmail.com>
2022-05-14 20:07:01 +0700

    Format code

--- src/database/migrations/1652521658261-add_relationship_auth_and_token.ts
+++ src/database/migrations/1652521658261-add_relationship_auth_and_token.ts
@@ -1,16 +1,19 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class addRelationshipAuthAndToken1652521658261 implements MigrationInterface {
-    name = 'addRelationshipAuthAndToken1652521658261'
+  name = 'addRelationshipAuthAndToken1652521658261';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE \`token\` ADD \`authId\` int NULL`);
-        await queryRunner.query(`ALTER TABLE \`token\` ADD CONSTRAINT \`FK_b807b83ea727332c8b2fad6f0ec\` FOREIGN KEY (\`authId\`) REFERENCES \`auth\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE \`token\` ADD \`authId\` int NULL`);
+    await queryRunner.query(
+      `ALTER TABLE \`token\` ADD CONSTRAINT \`FK_b807b83ea727332c8b2fad6f0ec\` FOREIGN KEY (\`authId\`) REFERENCES \`auth\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE \`token\` DROP FOREIGN KEY \`FK_b807b83ea727332c8b2fad6f0ec\``);
-        await queryRunner.query(`ALTER TABLE \`token\` DROP COLUMN \`authId\``);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE \`token\` DROP FOREIGN KEY \`FK_b807b83ea727332c8b2fad6f0ec\``,
+    );
+    await queryRunner.query(`ALTER TABLE \`token\` DROP COLUMN \`authId\``);
+  }
 }
