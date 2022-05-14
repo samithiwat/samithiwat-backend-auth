a39f6a06b1afba4fe2efff93f4db91b3631b2029
samithiwat <samithiwat2544@gmail.com>
2022-05-14 16:49:59 +0700

    Update credential dto

--- src/auth/dto/credential.dto.ts
+++ src/auth/dto/credential.dto.ts
@@ -1,1 +1,9 @@
-export class CredentialDto {}
+export class CredentialDto {
+  accessToken: string;
+  refreshToken: string;
+  expiresIn: number;
+
+  constructor(partial: Partial<CredentialDto>) {
+    Object.assign(this, partial);
+  }
+}
