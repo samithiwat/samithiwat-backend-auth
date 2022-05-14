460790ebed8eefe231d84c5f39ad0fdfaafb0f1d
samithiwat <samithiwat2544@gmail.com>
2022-05-14 16:49:17 +0700

    Update response dto

--- src/auth/dto/response.dto.ts
+++ src/auth/dto/response.dto.ts
@@ -2,11 +2,12 @@ import { HttpStatus } from '@nestjs/common';
 import { UserDto } from '../../user/user.interface';
 import { Auth } from '../entities/auth.entity';
 import { Token } from '../entities/token.entity';
+import { CredentialDto } from './credential.dto';
 
 export class ResponseDto {
   statusCode: HttpStatus;
   errors: string[];
-  data: Token | Auth | UserDto;
+  data: Token | Auth | UserDto | CredentialDto;
 
   constructor(partial: Partial<ResponseDto>) {
     Object.assign(this, partial);
