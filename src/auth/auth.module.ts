c07bdd8a2093ea2e8f91e23a1d16d897580eb89f
samithiwat <samithiwat2544@gmail.com>
2022-05-14 16:48:35 +0700

    Register user module to app module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from 'src/user/user.module';
 import { AuthController } from './auth.controller';
 import { Auth } from './entities/auth.entity';
 import { Token } from './entities/token.entity';
@@ -21,6 +22,7 @@ import { TokenService } from './services/token.service';
       inject: [ConfigService],
     }),
     TypeOrmModule.forFeature([Auth, Token]),
+    UserModule,
   ],
   controllers: [AuthController],
   providers: [AuthService, RefreshTokenService, JwtService, TokenService],
