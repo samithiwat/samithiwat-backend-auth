25ce6d65398d17e2b9f4b154076704a1ebf55cba
samithiwat <samithiwat2544@gmail.com>
2022-05-14 16:49:48 +0700

    Create register response interface

--- src/auth/interface/auth.interface.ts
+++ src/auth/interface/auth.interface.ts
@@ -1,5 +1,10 @@
 import { BaseResponse } from 'src/common/interface/base.interface';
+import { UserDto } from 'src/user/user.interface';
 import { CredentialDto } from '../dto/credential.dto';
+
+export interface RegisterResponse extends BaseResponse {
+  data: UserDto;
+}
 
 export interface RefreshTokenResponse extends BaseResponse {
   data: CredentialDto;
