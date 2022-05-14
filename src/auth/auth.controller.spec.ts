5d4dd2ce701a81651603bd309d0ffd99fa0a28d8
samithiwat <samithiwat2544@gmail.com>
2022-05-14 20:11:59 +0700

    Fix test suit error

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -2,13 +2,26 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
 import { AuthService } from './services/auth.service';
 
+const MockAuthService = {
+  register: jest.fn(),
+  login: jest.fn(),
+  validate: jest.fn(),
+  changePassword: jest.fn(),
+  refreshToken: jest.fn(),
+};
+
 describe('AuthController', () => {
   let controller: AuthController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
-      providers: [AuthService],
+      providers: [
+        {
+          provide: AuthService,
+          useValue: MockAuthService,
+        },
+      ],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
