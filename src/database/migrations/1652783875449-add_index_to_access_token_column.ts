f7a27f5283b17cfcfdfd1aee17c1cc1b0ce95a6f
samithiwat <samithiwat2544@gmail.com>
2022-05-17 18:44:29 +0700

    Format

--- src/database/migrations/1652783875449-add_index_to_access_token_column.ts
+++ src/database/migrations/1652783875449-add_index_to_access_token_column.ts
@@ -1,14 +1,15 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class addIndexToAccessTokenColumn1652783875449 implements MigrationInterface {
-    name = 'addIndexToAccessTokenColumn1652783875449'
+  name = 'addIndexToAccessTokenColumn1652783875449';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE INDEX \`IDX_d5b9f4694521b7fbb121aff385\` ON \`token\` (\`access_token\`)`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE INDEX \`IDX_d5b9f4694521b7fbb121aff385\` ON \`token\` (\`access_token\`)`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`DROP INDEX \`IDX_d5b9f4694521b7fbb121aff385\` ON \`token\``);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP INDEX \`IDX_d5b9f4694521b7fbb121aff385\` ON \`token\``);
+  }
 }
